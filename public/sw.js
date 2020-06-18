209007acc88e0567684cafef5919d755c26651fd
Fabio Keller <info@leadingweb.de>
2020-06-18 19:32:20 +0200

    design bug fix

--- public/sw.js
+++ public/sw.js
@@ -22,43 +22,18 @@ const staticAssets = [
     './welcome.js'
 ];
 
-self.addEventListener('install', async e => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(staticAssets);
-    return self.skipWaiting();
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(staticAssets);
+    })
+  );
 });
 
-self.addEventListener('activate', e => {
-    self.ClientRectList.claim();
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
-
-self.addEventListener('fetch', e => {
-    // console.log(e);
-    const req = e.request;
-    // console.log(req);
-    const url = new URL(req.url);
-
-    if(url.origin == location.origin) {
-        e.respondWith(cacheFirst(req));
-    }
-});
-
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = cache.match(req);
-    return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-    const cache = fetch(req);
-    try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-
-    }
-    catch(e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
-}
