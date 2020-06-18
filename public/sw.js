80779228aac4c7c31b8e891178c0d41912be5977
Fabio Keller <info@leadingweb.de>
2020-06-18 18:24:51 +0200

    testing PWA

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,10 @@
 const cacheName = 'quotative-v1';
-const assets = [
+const staticAssets = [
     './',
+    './assets/Bubble.js',
+    './assets/validation.js',
+    './assets/pic/logo512.png',
+    './assets/pic/logo192.png',
     './first-login.js',
     './login.js',
     './main.js',
@@ -26,5 +30,35 @@ self.addEventListener('install', async e => {
 
 self.addEventListener('activate', e => {
     self.ClientRectList.claim();
-})
+});
 
+self.addEventListener('fetch', e => {
+    // console.log(e);
+    const req = e.request;
+    // console.log(req);
+    const url = new URL(req.url);
+
+    if(url.origin == location.origin) {
+        e.respondWith(cacheFirst(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = cache.match(req);
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+    const cache = fetch(req);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+
+    }
+    catch(e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
